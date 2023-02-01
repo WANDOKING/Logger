bc2faa27193efe868f4b626a6a8d4668a890dffe
WANDOKING <108464645+WANDOKING@users.noreply.github.com>
2023-02-01 14:45:11 +0900

    Add files via upload

---
+++ Logger.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define Log(M) Logger::LogMessage((M), __FILE__, __LINE__)
+#define LogWithWSAError(M) Logger::LogMessageWithWSAError((M), __FILE__, __LINE__)
+#define Crash() Logger::RaiseCrash()
+
+#pragma warning(push)
+#pragma warning(disable: 6011) // raiseCrash 함수의 경고를 무시한다
+
+class Logger
+{
+public:
+	static void LogMessage(const char* message);
+	static void LogMessage(const char* message, const char* fileName, int line);
+	static void LogMessageWithWSAError(const char* message);
+	static void LogMessageWithWSAError(const char* message, const char* fileName, int line);
+
+	// intentional crash
+	inline static void RaiseCrash()
+	{
+		int* nullPointer = 0x00000000;
+		*nullPointer = 0;
+	}
+private:
+	Logger();
+	~Logger();
+
+	static void getCurrentTimeInfo(char* buffer);
+
+	static Logger mInstance;
+	static FILE* mLogFile;
+};
+
+#pragma warning(pop)
---
+++ Test.cpp
@@ -0,0 +1,52 @@
+﻿#include <cassert>
+#include <iostream>
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+
+#include "Logger.h"
+
+BOOL PrintDomainToIps(const WCHAR* domain)
+{
+	ADDRINFOW* addrInfo;
+	if (GetAddrInfo(domain, L"0", nullptr, &addrInfo) != 0)
+	{
+		LogWithWSAError("GetAddrInfo() error!");
+		Crash();
+	}
+
+	ADDRINFOW* traversal = addrInfo;
+	while (traversal != nullptr)
+	{
+		SOCKADDR_IN* sockAddr = (SOCKADDR_IN*)traversal->ai_addr;
+
+		WCHAR buffer[16] = { 0 };
+		if (InetNtop(AF_INET, &sockAddr->sin_addr, buffer, 16) == nullptr)
+		{
+			LogWithWSAError("InetNtop() error!");
+			Crash();
+		}
+
+		std::wcout << buffer << '\n';
+
+		traversal = traversal->ai_next;
+	}
+
+	FreeAddrInfo(addrInfo);
+
+	return TRUE;
+}
+
+int main(void)
+{
+	WSADATA wsaData;
+	
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		LogWithWSAError("WSAStartup() error!");
+		Crash();
+	}
+
+	PrintDomainToIps(L"gosoasgle.com");
+
+	WSACleanup();
+}
