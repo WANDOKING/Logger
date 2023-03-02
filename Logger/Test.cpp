134013f8d8d299278b3f9d21c14acb9ad14e4efd
WANDOKING <dowankim57@gmail.com>
2023-03-03 00:55:57 +0900

    version 2.0.0 * WindowsAPI 기반 파일 입출력 하도록 전면 개편 * 프로세스 실행중이더라도 로그 파일을 열어볼 수 있음 * 버퍼 길이에 대한 안전 장치를 대폭 추가 * 파일 입출력 실패에 대한 안전 장치를 대폭 추가 * UTF-16(LE) 파일로 저장하도록 수정 * LogWSAError() 함수를 따로 만들어 함수의 일관성을 지키도록 수정 * 메모리를 덜 쓰도록 수정

--- Logger/Test.cpp
+++ Logger/Test.cpp
@@ -1,12 +1,25 @@
-#pragma comment(lib, "ws2_32")
-
+#include <Windows.h>
 #include "Logger.h"
 
 int main(void)
 {
-	Logger::LogF(L"hello my name is %s", L"Minsu");
+	setlocale(LC_ALL, "korean");
 
-	LOG_WITH_WSAERROR(L"OK good");
+	LOG_WITH_WSAERROR(L"아니 뭔데");
+	LOG(L"Not Bad");
+	LOG(L"What are you doing man");
+	LOG_WITH_WSAERROR(L"아무말이나 하는중");
+	LOG_WITH_WSAERROR(L"영어랑 korean이랑도 섞어서 write");
+	LOG(L"숫자도192385712398");
 
-	LOG_WITH_WSAERROR(L"Not Bad");
+	int num = 5;
+	WCHAR name[] = L"김도완";
+	float floatNum = 1.62f;
+	LOGF(L"num = %d, name = %s, floatNum = %f", num, name, floatNum);
+
+	ASSERT_WITH_MESSAGE(1 > 0, L"1 > 0");
+	ASSERT_WITH_MESSAGE(2 > 0, L"2 > 0");
+	ASSERT_WITH_MESSAGE(3 > 0, L"3 > 0");
+	ASSERT_WITH_MESSAGE(4 > 0, L"4 > 0");
+	ASSERT_WITH_MESSAGE(5 > 0, L"5 > 0");
 }
